7cdaa80a920f1c040af07bef1af59d70965a5240
CoDan123 <let2350@gmail.com>
2021-10-27 14:18:48 -0600

    Added Header.js and Header.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import react, {useState, useEffect} from "react";
 import Movie from './Components/Movie';
-import { useEffect, useState } from 'react';
 
 const API_KEY = '087e6e53b047b687bcd13eb7475121ab';
   const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?api_key=087e6e53b047b687bcd13eb7475121ab&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate';
