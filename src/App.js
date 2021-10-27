6b57e15390842b9dd991a9e2896829d470482585
CoDan123 <let2350@gmail.com>
2021-10-26 23:12:30 -0600

    Added movies.map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import react from "react";
+import Movie from './Components/Movie';
 
 function App() {
+
+  const API_KEY = '087e6e53b047b687bcd13eb7475121ab';
+
   return (
     <div className="App">
-     
+      {movies.map(movie =>(
+        <Movie />
+      ))}
     </div>
   );
 }
