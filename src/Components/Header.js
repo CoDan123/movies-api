64d0402fa5da17815955b390c2846efecc6bee80
CoDan123 <let2350@gmail.com>
2021-10-27 14:22:37 -0600

    Added Header.js structure

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header-section'>
+            <div className="logo-container">
+
+            </div>
+            <div className="navigation">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header;
