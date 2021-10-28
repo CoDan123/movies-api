1bdd243b715369d27ce175e2f44d00c084c8e7f8
CoDan123 <let2350@gmail.com>
2021-10-28 09:10:59 -0600

    Corrected background image and Header positioning

--- src/Components/Hero-Section.js
+++ src/Components/Hero-Section.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './Hero-Section.css';
 import logo from "../Images/movies_background.png";
 
 const HeroSection = () => {
     return (
         <div className="hero-section">
-            <img src={logo}/>
+            
         </div>
     )
 }
