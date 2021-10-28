aef934a5f302409ba35a8b11065a2a2d4d842f47
CoDan123 <let2350@gmail.com>
2021-10-28 10:41:54 -0600

    Added search box in HeroSection

--- src/Components/Hero-Section.js
+++ src/Components/Hero-Section.js
@@ -5,7 +5,12 @@ import logo from "../Images/movies_background.png";
 const HeroSection = () => {
     return (
         <div className="hero-section">
-            
+            <div className="search-box">
+                <form>
+                    <input type="text" />
+                    <button></button>
+                </form>
+            </div>
         </div>
     )
 }
