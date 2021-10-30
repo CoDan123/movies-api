ebad6ee7d5390cecb80d8d005f5a2e658089bb09
CoDan123 <let2350@gmail.com>
2021-10-30 17:13:01 -0600

    Added search state

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ import HeroSection from './Components/Hero-Section';
 import Footer from "./Components/Footer";
 import Movie from './Components/Movie';
 
-const API_KEY = '087e6e53b047b687bcd13eb7475121ab';
+  const API_KEY = '087e6e53b047b687bcd13eb7475121ab';
   const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?api_key=087e6e53b047b687bcd13eb7475121ab&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate';
   const IMG_API = 'https://image.tmdb.org/t/p/w500';
   const SEARCH_API = 'https://api.themoviedb.org/3/search/company?api_key=087e6e53b047b687bcd13eb7475121ab&page=1&query='
 
   function App() {
     const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState('');
 
-    useEffect(() => {
-      fetch(FEATURED_API)
-    }, [])
-
+    
   
 
   return (
