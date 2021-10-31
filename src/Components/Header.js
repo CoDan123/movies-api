e5f65f55b01aee8ee1bd9e954d0d0d3e7506ab69
CoDan123 <let2350@gmail.com>
2021-10-30 23:03:13 -0600

    New Logo styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='header-section'>
             <div className="logo-container">
-                <h1>Movies<i class="fas fa-film"></i></h1>
+                <h1><i class="fas fa-film"></i>Movies</h1>
             </div>
             <div className="navigation">
                 <h3>Movies</h3>
