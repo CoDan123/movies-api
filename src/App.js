6a339b206717994a09e85b3c07bd1074377c1ab5
CoDan123 <let2350@gmail.com>
2021-11-03 17:39:47 -0600

    Imported axios

--- src/App.js
+++ src/App.js
@@ -4,61 +4,23 @@ import Header from './Components/Header';
 import HeroSection from './Components/Hero-Section';
 import Footer from "./Components/Footer";
 import Movie from './Components/Movie';
+import axios from 'axios';
 
-  const API_KEY = '087e6e53b047b687bcd13eb7475121ab';
-  const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?api_key=087e6e53b047b687bcd13eb7475121ab&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate';
-  const IMG_API = 'https://image.tmdb.org/t/p/w500';
-  const SEARCH_API = 'https://api.themoviedb.org/3/search/company?api_key=087e6e53b047b687bcd13eb7475121ab&page=1&query='
+  
+function App() {
 
-  function App() {
-    const [movies, setMovies] = useState([]);
-    const [search, setSearch] = useState('');
-    const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState();
 
-   
-
-    // const getMovies = async () => {
-    //     const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}`);
-    //     const data = await response.json();
-    //     setMovies(data.results);
-    // }
-
-    // const updateSearch = (e) => {
-    //   setSearch(e.target.value);
-    // }
-
-    // const getSearch = (e) => {
-    //   e.preventdefault();
-    //   setQuery(search);
-    //   setSearch('');
-    // }
   
 
-  return (
-    <div className="App">
-      
-      <Header />
-      <HeroSection 
-       
-       />
-      {/* {movies.length === 0 ?
-       <HeroSection 
-       search={search}
-       updateSearch={updateSearch} 
-       getMovies={getMovies}
-       />
-       : movies.map(result => (
-       <Movie 
-        key={result.results.id} 
-        title={result.results.title}
-        rating={result.results.vote_average}
-        />
-        ))
-      } */}
-      <Footer/>
-     
-    </div>
-  );
+    return (
+      <div className="App">
+        <Header />
+        <HeroSection/>
+        <Footer/>
+      </div>
+    );
 }
 
 export default App;
--- src/Components/Hero-Section.js
+++ src/Components/Hero-Section.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './Hero-Section.css';
 
-const HeroSection = () => {
+const HeroSection = (props) => {
     return (
         <div className="background">
             <div className="hero-image">
@@ -11,8 +11,8 @@ const HeroSection = () => {
             <div className="hero-search-overlay">
                 <div className="search-box">
                     <h1>Find your favorite movies, TV shows and more.</h1>
-                    <form >
-                        <input type="text" placeholder="Search here..."  />
+                    <form>
+                        <input type="text" placeholder="Search here..."/>
                         <button type="submit">Search</button>
                     </form>
                 </div>
