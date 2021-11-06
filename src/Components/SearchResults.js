b6c2b03c15e9a3977175cd69e9f9ef96559f925a
CoDan123 <let2350@gmail.com>
2021-11-05 18:13:23 -0600

    Result card styling updates

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -4,7 +4,7 @@ import './SearchResults.css';
 const SearchResults = (props) => {
     return (
         <div className="search-results-display">
-            <h1>{props.movieTitle}</h1>
+            {/* <h1>{props.movieTitle}</h1> */}
             <img src={props.basePosterPath + props.posterPath} alt="" />
         </div>
     )
