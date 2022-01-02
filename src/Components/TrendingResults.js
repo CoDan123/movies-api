47e5839d3258401b8f4391b25c5f1f252b6a8fbf
CoDan123 <let2350@gmail.com>
2022-01-02 12:43:11 -0700

    results display cleanup

--- src/Components/TrendingResults.js
+++ src/Components/TrendingResults.js
@@ -7,7 +7,7 @@ const TrendingResults = (props) => {
             <img src={props.basePosterPath + props.posterPath} alt="" />
             <div className="movie-over">
                 <h2>{props.movieTitle}</h2>
-                <h3>{props.voteAverage}</h3>
+                <h3>{props.voteAverage}/10</h3>
             </div>
         </div>
     )
