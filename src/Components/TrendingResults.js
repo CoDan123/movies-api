a678ec6d1cd07c404af4c9a531695bcd952c1216
CoDan123 <let2350@gmail.com>
2022-01-05 11:22:58 -0700

    Format cleanup

--- src/Components/TrendingResults.js
+++ src/Components/TrendingResults.js
@@ -3,10 +3,9 @@ import React from "react";
 const TrendingResults = (props) => {
     return (
         <div className="movie-card">
-            {/* <h1>{props.movieTitle}</h1> */}
             <img src={props.basePosterPath + props.posterPath} alt="" />
             <div className="movie-over">
-            <div className="titleRatingContainer">
+                <div className="titleRatingContainer">
                     <h2>{props.movieTitle}</h2>
                     <h3>{props.voteAverage}/10</h3>
                 </div>
